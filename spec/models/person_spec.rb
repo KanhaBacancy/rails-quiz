020ab91e26e39bcbf53aef7f5314b5a5f62702bb
kanhaiya <kanhaiya.dubey@bacancy.com>
2024-06-20 00:35:41 +0530

    Test: write test case for email validation

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -15,4 +15,10 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   it { is_expected.to belong_to(:company).optional }
+  subject { described_class.new(name: 'John Doe', phone_number: '1234567890', email: 'john.doe@example.com') }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should allow_value('john.doe@example.com').for(:email) }
+  it { should_not allow_value('john.doe').for(:email) }
 end
