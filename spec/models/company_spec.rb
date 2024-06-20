398bb42c73f778126863a73fb85832336b2594ca
kanhaiya <kanhaiya.dubey@bacancy.com>
2024-06-20 15:38:56 +0530

    Test: write rspec for model scope and for api.

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -12,4 +12,26 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   it { is_expected.to have_many :people }
+  describe '.by_name' do
+    let!(:company) { Company.create(name: 'DigitalOcean') }
+
+    context 'when name exists' do
+      it 'returns the company with the given name' do
+        expect(Company.by_name('Digital')).to include(company)
+      end
+    end
+
+    context 'when name does not exist' do
+      it 'returns an empty relation' do
+        expect(Company.by_name('Nonexistent')).to be_empty
+      end
+    end
+
+    context 'when name is nil' do
+      it 'returns all companies' do
+        other_company = Company.create(name: 'OtherCompany')
+        expect(Company.by_name(nil)).to match_array([company, other_company])
+      end
+    end
+  end
 end
