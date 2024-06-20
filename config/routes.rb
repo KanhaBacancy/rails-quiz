2c458be7ab5c5d419434843e0d1c55e6982e6cea
kanhaiya <kanhaiya.dubey@bacancy.com>
2024-06-20 16:32:01 +0530

    Api: make creation api with creation of multiple companies.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
 
   resources :people, only: [:index, :new, :create]
   root to: 'people#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :companies, only: [:create]
+    end
+  end
 end
